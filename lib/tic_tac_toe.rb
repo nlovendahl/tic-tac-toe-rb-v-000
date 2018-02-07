532f98459418d1f6798369483617c1a30aa445d2
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-07 01:24:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) == 0 || turn_count(board) % 2 = 0
+  if turn_count(board) == 0 || turn_count(board) % 2 == 0
     return "O"
   else
     return "X"
